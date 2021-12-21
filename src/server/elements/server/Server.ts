eae1897d9e3f5e8be7824bf47575c8728cb67271
Konrad Gadzinowski <kgadzinowski@gmail.com>
2021-12-21 16:38:18 +0100

    Added node types

--- src/server/elements/server/Server.ts
+++ src/server/elements/server/Server.ts
@@ -1,6 +1,8 @@
 import {ServerListener} from "./ServerListener";
 import {PageBase} from "../pages/base/PageBase";
 import {PageMain} from "../pages/main/PageMain";
+import process from 'process';
+import cluster from 'cluster';
 
 export class Server {
 
@@ -8,7 +10,6 @@ export class Server {
   server;
   debug = require('debug')('final:server');
   http = require('http');
-  cluster = require('cluster');
   numCPUs = require('os').cpus().length;
   isProduction: boolean;
   appListener: ServerListener = new ServerListener();
@@ -28,20 +29,20 @@ export class Server {
     self.server = self.http.createServer(self.getExpress());
 
     // Use multi-core on production
-    if (self.isProduction && self.cluster.isMaster) {
+    if (self.isProduction && cluster.isMaster) {
       console.log('Master cluster setting up ' + self.numCPUs + ' workers...');
       for (var i = 0; i < self.numCPUs; i++) {
-        self.cluster.fork();
+        cluster.fork();
       }
 
-      self.cluster.on('online', function (worker) {
+      cluster.on('online', function (worker) {
         console.log('Worker ' + worker.process.pid + ' is online');
       });
 
-      self.cluster.on('exit', function (worker, code, signal) {
+      cluster.on('exit', function (worker, code, signal) {
         console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
         console.log('Starting a new worker');
-        self.cluster.fork();
+        cluster.fork();
       });
 
     } else {
